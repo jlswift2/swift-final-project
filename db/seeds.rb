649a1f8a462531bdc4f73120223612c1be4274e0
Justin Swift <jlswift2@gmail.com>
2022-04-27 14:46:07 -0500

    Write seeds to fill database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Ingredient.destroy_all
+Tag.destroy_all
+Recipe.destroy_all
+RecipeTag.destroy_all
+RecipeIngredient.destroy_all
+
+5.times do 
+    User.create(username: (Faker::Color.color_name), email: (Faker::Internet.email), password: "test", password_confirmation: "test", first_name: (Faker::Emotion.noun), last_name: (Faker::Emotion.noun), friend_code: (SecureRandom.hex(5)))
+end
+
+40.times do 
+    Ingredient.create(name: Faker::Food.fruits )
+end
+
+10.times do
+    Tag.create(title: Faker::Color.color_name)
+end
+
+20.times do
+    Recipe.create(name: Faker::Food.fruits, description: Faker::GreekPhilosophers.quote, steps: [Faker::GreekPhilosophers.quote, Faker::GreekPhilosophers.quote, Faker::GreekPhilosophers.quote], prep_time: 2, total_time: 3, user: User.all.sample)
+end
+
+50.times do
+    RecipeTag.create(recipe: Recipe.all.sample, tag: Tag.all.sample)
+end 
+
+50.times do
+    RecipeIngredient.create(quantity: Faker::Number.between(from: 1, to: 10) , measurement: "cup", recipe: Recipe.all.sample, ingredient: Ingredient.all.sample)
+end
