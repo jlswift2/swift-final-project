c24f924cb02599e95a5f9551df809bc56e48d8ab
Justin Swift <jlswift2@gmail.com>
2022-04-27 13:49:48 -0500

    Generate models and migrations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has many :recipe_tags
+  has_many :tags, through: :recipe_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+    has_many :recipe_tags
+    has many :recipes, through: :recipe_tags
 end
