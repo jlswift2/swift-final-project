77a989261737f3c13e57f874e972349b91dc661f
Justin Swift <jlswift2@gmail.com>
2022-04-27 16:09:26 -0500

    Create Follow table to let users follow others

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_184830) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_195039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
