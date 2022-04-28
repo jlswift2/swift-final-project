20cd812fa5e5fca0cb1445ba3c2aa55142904d50
Justin Swift <jlswift2@gmail.com>
2022-04-28 09:57:04 -0500

    Add User and Recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,9 @@ class Recipe < ApplicationRecord
 
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :steps, presence: true
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
 
     has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
     has_many :followers, through: :following_users
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
