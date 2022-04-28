defb60f06df7401f8084c931ee30c217eb162555
Justin Swift <jlswift2@gmail.com>
2022-04-28 10:13:44 -0500

    Rectified route locations

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import { Routes, Route } from "react-router-dom";
 import { Outlet } from "react-router-dom"
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup'
 
 
 function App() {
 
   return (
-    <Outlet />
+    <div>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+
+        </Route>
+      </Routes>
+      <Outlet />
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-
-      </Route>
-    </Routes>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
