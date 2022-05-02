43fff1464d4d94097195cb1ba433a7a89381f4b2
Justin Swift <jlswift2@gmail.com>
2022-05-02 13:00:32 -0500

    Render tags on recipe cards

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :steps, :prep_time, :total_time, :user_id
+  
+  has_many :tags
 end
--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 function RecipeCard({ recipe }) {
+  
+  let tags = recipe.tags.map(tag => {
+    return(tag.title)
+  })
+
   return (
     <div>
       <h3>{recipe.name}</h3>
       <h4>{recipe.description}</h4>
-      <h6>{recipe.tags}</h6>
+      <h6>{tags}</h6>
     </div>
   )
 }
