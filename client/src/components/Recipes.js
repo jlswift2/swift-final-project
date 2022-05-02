2d3a12287dd2f6718dce58570ae73480bcbd96af
Justin Swift <jlswift2@gmail.com>
2022-05-02 14:50:33 -0500

    Remove commented code

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -4,33 +4,16 @@ import RecipeCard from './RecipeCard';
 function Recipes( {user} ) {
   const [recipes, setRecipes] = useState([])
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setIsLoading(true);
-  //     setHasError(false);
-  //     try {
-  //       const res = await fetch(`/recipes/${user.id}`,
-  //       );
-  //       const json = await res.json()
-  //       setRecipes(json.hits);
-  //     } catch (error) {
-  //       setHasError(true)
-  //     }
-  //     setIsLoading(false);
-  //   };
-  //   fetchData();
-  // }, [setRecipes]);
-
   useEffect( () => {
     if (user) {
-      fetch(`/recipes/${user.id}`)
+      fetch(`/user/${user.id}/recipes`)
       .then(resp => resp.json())
       .then(data => setRecipes(data))
     }
   }, [user])
 
   const displayRecipes = recipes.map(recipe => {
-    return(<RecipeCard recipe={recipe}/>)})
+    return(<RecipeCard key={recipe.id} recipe={recipe}/>)})
 
   return (
     <div>
