899395205e2e891ad172394bb3624c29d1ffa101
Justin Swift <jlswift2@gmail.com>
2022-05-02 14:50:20 -0500

    Add navigation to individual Recipe page

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 function RecipeCard({ recipe }) {
-  
+  let navigate = useNavigate()
   let tags = recipe.tags.map(tag => {
     return(tag.title)
   })
 
+  function handleClick() {
+    navigate(`/recipes/${recipe.id}`)
+  }
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <h3>{recipe.name}</h3>
       <h4>{recipe.description}</h4>
       <h6>{tags}</h6>
