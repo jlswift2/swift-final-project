b12e7b3fee36596b3604aa639d4d28cb01ca2ce8
Justin Swift <jlswift2@gmail.com>
2022-05-02 09:25:42 -0500

    Add greeting for users

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Greeting from '../components/Greeting';
 import Recipes from '../components/Recipes';
 
-function LandingPage () {
+function LandingPage ({ user }) {
   return (
     <div>
-        <Greeting />
+        <Greeting user={user}/>
         <Recipes />
     </div>
   )
