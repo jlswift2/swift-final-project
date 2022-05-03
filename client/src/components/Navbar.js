152c811663ddeed7754eb4d9982db502418dce67
Justin Swift <jlswift2@gmail.com>
2022-05-03 14:58:05 -0500

    Add LandingPage navigation to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import LogoutButton from './LogoutButton'
 
 function Navbar({ user, setUser }) {
     return (
         <nav>
             <LogoutButton user={user} setUser={setUser}/>
+            {user ? <Link to="/land">My Landing Page</Link> : <Link to="/login">My Landing Page</Link>}
         </nav>
     )
 }
