135f1c9f1bbc2d307b46c10dda6300e05bbcf2d1
Justin Swift <jlswift2@gmail.com>
2022-05-04 19:33:45 -0500

    Add TagForm to allow the addition of personal tags

--- client/src/components/TagsForm.js
+++ client/src/components/TagsForm.js
@@ -1,9 +1,50 @@
 import React from 'react'
 
 function TagsForm({ tags, handleTagChange, handleTagAdd, handleTagRemove}) {
-  return (
-    <div>TagsForm</div>
-  )
+    
+    function onChange (e, index) {
+        handleTagChange(e, index)
+    }
+
+    function handleAdd () {
+        handleTagAdd()
+    }
+
+    function handleRemove (index) {
+        handleTagRemove(index)
+    }
+
+    return (
+        <div>
+            Tags Form
+            <form>
+                {tags.map((value, index) => (
+                    <div key={index}>
+                        <input
+                            type="text"
+                            name="tag"
+                            value={value}
+                            onChange={(e) => onChange(e, index)}
+                            required
+                        />
+                        {tags.length - 1 === index &&
+                        (
+                            <button type="button" onClick={handleAdd}>
+                                <span>Add a Tag</span>
+                            </button>
+                        )}
+                        {tags.length > 1 &&
+                        (
+                            <button type="button" onClick={() => handleRemove(index)}>
+                                <span>Remove</span>
+                            </button>
+                        )}
+                    </div>
+                    ))}
+            </form>
+
+        </div>
+    )
 }
 
 export default TagsForm
