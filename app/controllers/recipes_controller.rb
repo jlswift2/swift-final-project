3ed2b4565092a4745e38984c6f7aaecaeb0d1c22
Justin Swift <jlswift2@gmail.com>
2022-05-05 18:14:03 -0500

    Add create method for recipes/tags/ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,8 +19,55 @@ class RecipesController < ApplicationController
         render json: recipes, status: :ok
     end
 
-    def create 
-        byebug
+    def create
+        recipe_ingredients = params[:ingredients]
+        recipe_tags = params[:tags]
+        recipe = Recipe.create!(
+            name: params[:name],
+            description: params[:description],
+            steps: params[:steps],
+            prep_time: params[:prep_time],
+            total_time: params[:total_time],
+            user_id: session[:user_id]
+        )
+
+        recipe_ingredients.each do |ing|
+            ingred = Ingredient.find_by(name: ing[:ingredient])
+            if ingred
+                RecipeIngredient.create!(
+                    quantity: ing[:quantity], 
+                    measurement: ing[:measurement],
+                    ingredient_id: ingred[:id],
+                    recipe_id: recipe[:id]
+                ) 
+            else
+                new_ingredient = Ingredient.create!(name: ing[:ingredient])
+                RecipeIngredient.create!(
+                    quantity: ing[:quantity], 
+                    measurement: ing[:measurement],
+                    ingredient_id: new_ingredient[:id],
+                    recipe_id: recipe[:id]
+                )
+            end
+        end
+
+        recipe_tags.each do |tag|
+            already = Tag.find_by(title: tag)
+            if already
+                RecipeTag.create!(
+                    recipe_id: recipe[:id],
+                    tag_id: already[:id]
+                )
+            else
+                new_tag = Tag.create!(title: tag)
+                RecipeTag.create!(
+                    recipe_id: recipe[:id],
+                    tag_id: new_tag[:id]
+                )
+            end
+        end
+
+        render json: recipe, status: :created
     end
 
     private
