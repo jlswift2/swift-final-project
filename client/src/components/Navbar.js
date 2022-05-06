101a39de74deecbaf4d5808c303a721ba9b07d25
Justin Swift <jlswift2@gmail.com>
2022-05-06 10:49:35 -0500

    Add navigation to RecipeBook on Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,6 +7,7 @@ function Navbar({ user, setUser }) {
         <nav>
             <LogoutButton user={user} setUser={setUser}/>
             {user ? <Link to="/land">My Landing Page</Link> : <Link to="/login">My Landing Page</Link>}
+            {user ? <Link to="/recipes">My Recipe Book</Link> : <Link to="/login">My Recipe Book</Link>}
         </nav>
     )
 }
