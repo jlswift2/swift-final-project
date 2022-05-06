e29420b831ac85f975f9cbea7f38e11cf424e960
Justin Swift <jlswift2@gmail.com>
2022-05-06 10:39:13 -0500

    Render SearchBar on the page

--- client/src/pages/RecipesBook.js
+++ client/src/pages/RecipesBook.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RBRecipes from '../components/RBRecipes'
 import { Link } from 'react-router-dom'
+import SearchBar from '../components/SearchBar'
 
 function RecipesBook({ user }) {
+  const [query, setQuery] = useState("")
+  
   return (
     <div>
       <h1>RecipesPage</h1>
       <Link to="/land">Return to your landing page</Link>
-      <RBRecipes user={user}/>
+      <SearchBar setQuery={setQuery} />
+      <RBRecipes user={user} query={query}/>
     </div>
   )
 }
