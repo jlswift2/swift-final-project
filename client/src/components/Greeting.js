b5fe5d8a2ccad2ff23d58075a073fb4917a2a1bd
Justin Swift <jlswift2@gmail.com>
2022-05-07 17:55:59 -0500

    Add styling to the greeting

--- client/src/components/Greeting.js
+++ client/src/components/Greeting.js
@@ -4,8 +4,8 @@ function Greeting({ user }) {
     
     
     return (
-        <div>
-            {user ? <h2>Welcome, {user.username}</h2> : <h2>Welcome</h2>}
+        <div className='font-serif italic text-4xl pt-5 pb-5 bg-slate-400 font-bold text-primary mt-4 mb-12 text-right'>
+            {user ? <h2 className='pr-8'>Welcome, {user.username}</h2> : <h2 className='pr-5'>Welcome</h2>}
         </div>
   )
 }
