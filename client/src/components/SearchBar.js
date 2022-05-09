080fdeea4d921b21f5c3d40afa65fa7d850c20e2
Justin Swift <jlswift2@gmail.com>
2022-05-09 11:37:11 -0500

    Add styling to RecipeBook page

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 function SearchBar({ setQuery }) {
   return (
-    <div>
-      <input placeholder="Enter Recipe Name or Tag" onChange={e => setQuery(e.target.value)} />
-    </div>
+    <span className="pl-10">
+      <input className="font-serif content-right w-1/3 p-2 align-right text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4" placeholder="Enter Recipe Name or Tag" onChange={e => setQuery(e.target.value)} />
+    </span>
   )
 }
 
