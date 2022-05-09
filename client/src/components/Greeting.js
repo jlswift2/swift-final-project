b98822e14cfbe66601bb269629d4eeec9d7bf8aa
Justin Swift <jlswift2@gmail.com>
2022-05-08 21:33:07 -0500

    Change greeting text color

--- client/src/components/Greeting.js
+++ client/src/components/Greeting.js
@@ -4,7 +4,7 @@ function Greeting({ user }) {
     
     
     return (
-        <div className='font-serif italic text-4xl pt-5 pb-5 bg-slate-400 font-bold text-primary mt-4 mb-12 text-right'>
+        <div className='font-serif italic text-4xl text-white pt-5 pb-5 bg-slate-400 font-bold text-primary mt-4 mb-12 text-right'>
             {user ? <h2 className='pr-8'>Welcome, {user.username}</h2> : <h2 className='pr-5'>Welcome</h2>}
         </div>
   )
