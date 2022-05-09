3072ab2665f407136ec2ff3ba8cd94326f11e67e
Justin Swift <jlswift2@gmail.com>
2022-05-08 21:32:27 -0500

    Add new recipe button and styling

--- client/src/components/LPRecipes.js
+++ client/src/components/LPRecipes.js
@@ -19,9 +19,16 @@ function LPRecipes( {user} ) {
 
   return (
     <div>
-      <h2>Recipes</h2>
-      <button onClick={() => navigate("/recipes/new")} >New Recipe</button>
-      {displayRecipes}
+      <h2 className="font-serif mb-6 text-xl md:text-4xl uppercase font-bold flex justify-center">Recipes</h2>
+      <button className="text-green-600 pl-10" onClick={() => navigate("/recipes/new")}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+        </svg>
+        New Recipe
+      </button>
+      <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 justify-center">
+        {displayRecipes}
+      </div>
     </div>
   )
 }
