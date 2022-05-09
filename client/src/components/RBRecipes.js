dcf9ca00460dc13da61f53c124cecd5aa5e0986d
Justin Swift <jlswift2@gmail.com>
2022-05-09 11:37:00 -0500

    Add styling to RecipeBook page

--- client/src/components/RBRecipes.js
+++ client/src/components/RBRecipes.js
@@ -27,8 +27,8 @@ function RBRecipes( {user, query} ) {
     return(<RecipeCard key={recipe.id} recipe={recipe} />)})
 
   return (
-    <div>
-      {displayRecipes}
+    <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 justify-center">
+        {displayRecipes}
     </div>
   )
 }
