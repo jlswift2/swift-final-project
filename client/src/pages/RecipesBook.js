fdd7e3b3a8a78c1e7e1c825743deb7581566c508
Justin Swift <jlswift2@gmail.com>
2022-05-09 11:37:45 -0500

    Add styling to RecipeBook page and move components

--- client/src/pages/RecipesBook.js
+++ client/src/pages/RecipesBook.js
@@ -6,13 +6,18 @@ import SearchBar from '../components/SearchBar'
 function RecipesBook({ user }) {
   const [query, setQuery] = useState("")
   let navigate = useNavigate()
-  
+
   return (
-    <div>
-      <Link to="/land">Return to your landing page</Link>
-      <h2 className="font-serif mb-6 text-xl md:text-4xl uppercase font-bold flex justify-center">My Recipe Book</h2>
+    <div className="mt-10">
+      <Link className="text-gray-800" to="/land">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+        </svg>
+        Return to your landing page
+      </Link>
+      <h2 className="font-serif mt-4 mb-6 text-xl md:text-4xl uppercase font-bold flex justify-center">My Recipe Book</h2>
       <SearchBar setQuery={setQuery} />
-      <button className="text-green-600 pl-10" onClick={() => navigate("/recipes/new")}>
+      <button className="text-green-600 pr-10 float-right" onClick={() => navigate("/recipes/new")}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
         </svg>
