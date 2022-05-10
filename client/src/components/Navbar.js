b9cfc1188d4fb825d70b920ac3495010ab6ee0c4
Justin Swift <jlswift2@gmail.com>
2022-05-10 12:29:57 -0500

    Add sign up button for navigation ease

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,6 +7,12 @@ function Navbar({ user, setUser }) {
         <nav className="absolute top-[25%] h-1/2 w-full flex align-middle overflow-y-auto px-3 bg-gray-50 rounded dark:bg-gray-800" >
             <div className="m-auto w-full text-center">
                 <LogoutButton user={user} setUser={setUser}/>
+                {user ? null 
+                    : 
+                    <Link to="/signup" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <span className="flex-1 ml-3 whitespace-nowrap text-xl">Sign Up</span>
+                    </Link>
+                }
                 {user ? <Link to="/land" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                     <span className="flex-1 ml-3 whitespace-nowrap text-xl">My Landing Page</span>
                     </Link> 
