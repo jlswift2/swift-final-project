449ce2a8480767bfd140fd2ddbff6dcf3fc1840b
Justin Swift <jlswift2@gmail.com>
2022-05-10 22:03:40 -0500

    Add update and delete endpoints for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get "/user/:id/recipes/all", to: "recipes#user_recipes_all"
   get "/recipes/:recipeId", to: "recipes#show"
   post "/recipes/new", to: "recipes#create"
+  delete "recipes/:recipeId", to: "recipes#delete"
+  patch "recipes/:recipeId", to: "recipes#update"
 
   get "/user/:id/follows", to: "follows#user_follows"
 end
