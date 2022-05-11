fccbfe9c46324278ba656180e7d2cff92e6138cb
Justin Swift <jlswift2@gmail.com>
2022-05-11 16:37:08 -0500

    Style Tags for constitency

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -6,7 +6,7 @@ function RecipeCard({ recipe }) {
   let navigate = useNavigate()
   let tags = recipe.tags.map(tag => {
     return(
-      <span key={tag.id} className="text-xs float-right font-semibold inline-block py-1 px-2 uppercase rounded text-violet-600 bg-violet-200 last:ml-0 ml-1">
+      <span key={tag.id} className="text-xs float-right font-semibold  py-1 px-2 uppercase rounded text-violet-600 bg-violet-200 last:ml-0 ml-1">
         {tag.title}
       </span>
     )
@@ -17,11 +17,15 @@ function RecipeCard({ recipe }) {
   }
 
   return (
-    <div className="rounded overflow-hidden shadow-md shadow-gray-600" onClick={handleClick}>
-      <h3 className="font-serif mb-6 text-xl md:text-xl font-bold flex justify-center">{recipe.name}</h3>
-      <h4 className="font-serif italic text-lg flex justify-left">{recipe.description}</h4>
+    <div className="flex flex-col justify-between p-8 transition-shadow bg-white rounded overflow-hidden shadow-gray-600 shadow-xl group hover:shadow-lg" onClick={handleClick}>
       <div>
-        {tags}
+        <h3 className="font-serif text-xl font-bold text-center">{recipe.name}</h3>
+        <div class="border-t-2 border-indigo-100">
+          <h4 className="font-serif mt-4 italic text-lg justify-left">{recipe.description}</h4>
+        </div>
+      </div>
+      <div class="inline-flex items-center mt-16 text-indigo-600 justify-end">
+        <p class="text-lg font-medium">{tags}</p>
       </div>
     </div>
   )
