75e24b8a4f5230143e36f6be6d889f01e852c77e
Justin Swift <jlswift2@gmail.com>
2022-05-18 16:57:12 -0500

    Render Friend's Recipe Book page

---
+++ client/src/pages/FriendRBPage.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import FriendRBRecipes from '../components/FriendRBRecipes'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import SearchBar from '../components/SearchBar'
+
+function FriendRBPage({ user }) {
+  const [query, setQuery] = useState("")
+  const [friend, setFriend] = useState({})
+  let navigate = useNavigate()
+
+  return (
+    <div className="mt-10">
+      <Link className="text-gray-800" to="/land">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+        </svg>
+        Return to your landing page
+      </Link>
+      <h2 className="font-serif mt-4 mb-6 text-xl md:text-4xl uppercase font-bold flex justify-center">{friend.username}'s Recipe Book</h2>
+      <SearchBar setQuery={setQuery} />
+      <FriendRBRecipes user={user} query={query} setFriend={setFriend} />
+    </div>
+  )
+}
+
+export default FriendRBPage
