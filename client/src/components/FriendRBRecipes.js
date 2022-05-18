4a14d37e7092099640fd84fbc02226690d9e309d
Justin Swift <jlswift2@gmail.com>
2022-05-18 16:56:52 -0500

    Add filter and styling to the recipes component

--- client/src/components/FriendRBRecipes.js
+++ client/src/components/FriendRBRecipes.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
 
-function FriendRBRecipes({ user }) {
-    const [recipes, setRecipes] = useState(null)
-    const [friend, setFriend] = useState({})
+function FriendRBRecipes({ user, query, setFriend }) {
+    const [recipes, setRecipes] = useState([])
+    // const [friend, setFriend] = useState({})
     const { friendId } = useParams()
 
     useEffect( () => {
@@ -25,11 +25,23 @@ function FriendRBRecipes({ user }) {
         // }
     }, [friendId])
 
+    const filteredRecipes = recipes.filter(recipe => {
+        if (recipe.name === "") {
+          return recipe;
+        } else if (recipe.name.toLowerCase().includes(query.toLowerCase())) {
+          return recipe;
+        } else if (recipe.tags.find(tag => tag.title.toLowerCase().includes(query.toLowerCase()))) {
+          return recipe;
+        }
+      })
+    
+    const displayRecipes = filteredRecipes.map(recipe => {
+        return(<RecipeCard key={recipe.id} recipe={recipe} />)})
+    
     return (
-    <div>
-        <h2>{friend.username}'s Recipes</h2>
-        {recipes ? recipes.map(recipe => <RecipeCard key={recipe.id} recipe={recipe} />) : "loading now..."}
-    </div>
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 justify-center">
+            {displayRecipes}
+        </div>
     )
 }
 
