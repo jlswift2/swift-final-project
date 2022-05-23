64b00715ac3e45b9967014bbf0773047b4b4bc86
Justin Swift <jlswift2@gmail.com>
2022-05-22 20:28:48 -0500

    Create container for Friends List

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -17,7 +17,7 @@ function FriendsList({ user }) {
     })
 
     return (
-        <div className="w-1/2 m-auto">
+        <div className="w-1/2 m-auto flex flex-col justify-between p-8 transition-shadow border-2 bg-white rounded overflow-hidden shadow-slate-400 shadow-lg group">
             <h3 className="font-serif mb-6 text-xl md:text-4xl uppercase font-bold flex justify-center">Friends List</h3>
             <div className="grid grid-cols-3 gap-4 place-items-center">
                 {friend}
