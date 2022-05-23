4af60cadde6d529092444e821f5f1346eef8cfe3
Justin Swift <jlswift2@gmail.com>
2022-05-22 20:23:53 -0500

    Change friend's list color

--- client/src/components/Friend.js
+++ client/src/components/Friend.js
@@ -5,7 +5,7 @@ function Friend({ friend }) {
     let link = `/recipes/friends/${friend.id}`
   
     return (
-        <div className="font-serif mb-6 text-xl md:text-xl flex justify-center">
+        <div className="font-serif mb-6 text-xl md:text-xl flex justify-center text-green-800">
             <Link to={link}>{friend.username}</Link>
         </div>
     )
